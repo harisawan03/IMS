43a39c141512977a73af95cea80c70adeeae1f8c
Hairs Awan <harisawan@rvc.local>
2019-08-16 08:52:32 -0500

    completed posting functionality of item removal

--- PHP/remove.php
+++ PHP/remove.php
@@ -3,7 +3,11 @@
 $file = 'db-connect.php';
 include $file;
 
-$sqlremove = "UPDATE it_inventory SET owned = owned - 1, available = available - 1 WHERE id = 1"; // figure out how removal will work if item is associated with an employee
+$amountRemoved = $_POST['amount'];
+
+$sqlremove = "UPDATE it_inventory SET owned = owned - $amountRemoved, available = available - $amountRemoved WHERE id = 1"; // figure out how removal will work if item is associated with an employee
 $sqldata = sqlsrv_query($conn, $sqlremove) or die( print_r( sqlsrv_errors(), true));
+
+header("Location: /../confirm-remove.html");
 
 ?>
