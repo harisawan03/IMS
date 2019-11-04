9fb58803aac5dc1b9a1b29897fc7392f78824777
Haris <harisawan03@gmail.com>
2019-11-04 13:04:02 -0600

    add localStorage and test separate php var

--- PHP/get-inventory.php
+++ PHP/get-inventory.php
@@ -6,7 +6,7 @@ include $file;
 $upc = 1;
 // <script>sessionStorage.getItem('upc');</script>;
 
-$sqlget = "SELECT item, owned, available FROM it_inventory WHERE id = `$upc`";
+$sqlget = "SELECT item, owned, available FROM it_inventory WHERE id = {$upc}";
 $sqldata = sqlsrv_query($conn, $sqlget) or die( print_r( sqlsrv_errors(), true));
 $inventory = sqlsrv_fetch_array($sqldata);
 
