c96399ede0d7bd783b574460fdb9ab2cb9e0abdb
Haris <harisawan03@gmail.com>
2019-12-09 11:48:57 -0600

    parameter fix attempt

--- PHP/get-inventory.php
+++ PHP/get-inventory.php
@@ -8,19 +8,19 @@ echo $upc;
 echo $_COOKIE + 'cookie';
 
 
-// $sqlget = "SELECT item, owned, available FROM it_inventory WHERE upc LIKE (?)";
-// $params = ($upc);
-// $sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
-// $inventory = sqlsrv_fetch_array($sqldata);
+$sqlget = "SELECT item, owned, available FROM it_inventory WHERE upc LIKE (?)";
+$params = ($upc);
+$sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
+$inventory = sqlsrv_fetch_array($sqldata);
 
-// echo "<p>";
-// echo $inventory['item'];
-// echo "</p>";
-// echo "<p>Total Owned: ";
-// echo $inventory['owned'];
-// echo "</p>";
-// echo "<p>Total Available: ";
-// echo $inventory['available'];
-// echo "</p>";
+echo "<p>";
+echo $inventory['item'];
+echo "</p>";
+echo "<p>Total Owned: ";
+echo $inventory['owned'];
+echo "</p>";
+echo "<p>Total Available: ";
+echo $inventory['available'];
+echo "</p>";
 
 ?>
