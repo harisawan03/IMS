9a8884ae09df2d0e5796e8a4ae4d2b8c98199797
Haris <harisawan03@gmail.com>
2019-12-09 12:52:24 -0600

    parameter fix attempt

--- PHP/update-in.php
+++ PHP/update-in.php
@@ -2,6 +2,10 @@
 $file = 'db-connect.php';
 include $file;
 
-$sqlupdate = "UPDATE it_inventory SET available = available + 1 WHERE id = 1";
-$sqldata = sqlsrv_query($conn, $sqlupdate) or die( print_r( sqlsrv_errors(), true));
+$sqlupdate = "UPDATE it_inventory SET available = available + 1 WHERE upc LIKE (?)";
+$params = array($upc);
+$sqldata = sqlsrv_query($conn, $sqlget, $params);
+if ($sqldata === false) {
+    die( print_r( sqlsrv_errors(), true));
+}
 ?>
