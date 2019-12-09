c100bc924c4f403460cfcd029e326487da3d212b
Haris <harisawan03@gmail.com>
2019-12-09 11:17:53 -0600

    parameter fix attempt

--- PHP/get-inventory.php
+++ PHP/get-inventory.php
@@ -5,6 +5,7 @@ include $file;
 
 // $upc = $_COOKIE["upc"];
 
+echo $_GET['upc'];
 $sqlget = "SELECT item, owned, available FROM it_inventory WHERE upc LIKE (?)";
 $params = ($_GET['upc']);
 $sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
