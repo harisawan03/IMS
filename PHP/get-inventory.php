5998fcdb917f63c64f0d3965620f6bd8f6aaa657
Haris <harisawan03@gmail.com>
2019-12-09 11:47:11 -0600

    parameter fix attempt

--- PHP/get-inventory.php
+++ PHP/get-inventory.php
@@ -13,14 +13,14 @@ echo $_COOKIE + 'cookie';
 // $sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
 // $inventory = sqlsrv_fetch_array($sqldata);
 
-echo "<p>";
-echo $inventory['item'];
-echo "</p>";
-echo "<p>Total Owned: ";
-echo $inventory['owned'];
-echo "</p>";
-echo "<p>Total Available: ";
-echo $inventory['available'];
-echo "</p>";
+// echo "<p>";
+// echo $inventory['item'];
+// echo "</p>";
+// echo "<p>Total Owned: ";
+// echo $inventory['owned'];
+// echo "</p>";
+// echo "<p>Total Available: ";
+// echo $inventory['available'];
+// echo "</p>";
 
 ?>
