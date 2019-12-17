b4378d24edcc459a16f4e4af17cc20838e4fe7b0
Haris <harisawan03@gmail.com>
2019-12-17 09:24:19 -0600

    fix copy/paste issue

--- PHP/update-in.php
+++ PHP/update-in.php
@@ -6,7 +6,7 @@ $upc = $_COOKIE["upc"];
 
 $sqlupdate = "UPDATE it_inventory SET available = available + 1 WHERE upc LIKE (?)";
 $params = array($upc);
-$sqldata = sqlsrv_query($conn, $sqlget, $params);
+$sqldata = sqlsrv_query($conn, $sqlupdate, $params);
 if ($sqldata === false) {
     die( print_r( sqlsrv_errors(), true));
 }
