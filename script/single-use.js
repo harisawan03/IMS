d660a517399bcd5d8f0a9c45d167c7d11b3ab16a
Haris <harisawan03@gmail.com>
2019-12-17 10:26:45 -0600

    add cookie functionality to displayData() JS function

--- script/single-use.js
+++ script/single-use.js
@@ -38,6 +38,9 @@ function remove() {
 let state;
 
 function displayData() {
+  $(document).ready(function () { 
+    createCookie("upc", sessionStorage.getItem('upc'), "10"); 
+  }); 
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
