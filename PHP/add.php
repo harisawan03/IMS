ad762ffcbac3d6a5b69875f681cc8c8ab66e12ea
Haris <harisawan03@gmail.com>
2019-12-17 09:35:50 -0600

    add cookie functionality to add.php for 'exists' case

--- PHP/add.php
+++ PHP/add.php
@@ -3,17 +3,20 @@
 $file = 'db-connect.php';
 include $file;
 
+$upc = $_COOKIE["upc"];
+
 $amountAdded = $_POST['amount'];
 $item = $_POST['item'];
 $category = $_POST['category'];
 
 if ($_POST['exists'] == 'Yes') {
-  $sqladd = "UPDATE it_inventory SET owned = owned + $amountAdded, available = available + $amountAdded WHERE id = 1";
+  $sqladd = "UPDATE it_inventory SET owned = owned + $amountAdded, available = available + $amountAdded WHERE upc LIKE (?)";
 } else {
   $sqladd = "INSERT INTO it_inventory (id, item, category, owned, bin, available) VALUES (19, '$item', '$category', $amountAdded, 19, $amountAdded)";
 }
 
-$sqldata = sqlsrv_query($conn, $sqladd) or die( print_r( sqlsrv_errors(), true));
+$params = array($upc);
+$sqldata = sqlsrv_query($conn, $sqladd, $params) or die( print_r( sqlsrv_errors(), true));
 
 header("Location: /../confirm-add.html");
 
