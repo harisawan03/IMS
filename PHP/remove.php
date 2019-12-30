bc5dea7a0ae7f79a303d43fe1e00887aaec22e82
Haris <harisawan03@gmail.com>
2019-12-30 11:14:36 -0600

    extend cookie to remove.php

--- PHP/remove.php
+++ PHP/remove.php
@@ -3,10 +3,13 @@
 $file = 'db-connect.php';
 include $file;
 
+$upc = $_COOKIE["upc"];
+
 $amountRemoved = $_POST['amount'];
 
-$sqlremove = "UPDATE it_inventory SET owned = owned - $amountRemoved, available = available - $amountRemoved WHERE id = 1"; // figure out how removal will work if item is associated with an employee
-$sqldata = sqlsrv_query($conn, $sqlremove) or die( print_r( sqlsrv_errors(), true));
+$sqlremove = "UPDATE it_inventory SET owned = owned - $amountRemoved, available = available - $amountRemoved WHERE upc LIKE (?)"; // figure out how removal will work if item is associated with an employee
+$params = array($upc);
+$sqldata = sqlsrv_query($conn, $sqlremove, $params) or die( print_r( sqlsrv_errors(), true));
 
 header("Location: /../confirm-remove.html");
 
