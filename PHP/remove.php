e3937b023d7523f515e6f099571a422697b3ddba
Haris <harisawan03@gmail.com>
2019-12-31 11:37:35 -0600

    prevent error message from briefly showing when adding and removing

--- PHP/remove.php
+++ PHP/remove.php
@@ -9,7 +9,7 @@ $amountRemoved = $_POST['amount'];
 
 $sqlremove = "UPDATE it_inventory SET owned = owned - $amountRemoved, available = available - $amountRemoved WHERE upc LIKE (?)"; // figure out how removal will work if item is associated with an employee
 $params = array($upc);
-$sqldata = sqlsrv_query($conn, $sqlremove, $params) or die( print_r( sqlsrv_errors(), true));
+$sqldata = sqlsrv_query($conn, $sqlremove, $params) //or die( print_r( sqlsrv_errors(), true));
 
 header("Location: /../confirm-remove.html");
 
