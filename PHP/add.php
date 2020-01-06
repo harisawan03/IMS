25ed9b47172a130fd1fab52a6f0cf668b7d1d3a5
Haris <harisawan03@gmail.com>
2020-01-06 10:36:15 -0600

    alert user if attempting to add duplicate upc

--- PHP/add.php
+++ PHP/add.php
@@ -13,15 +13,16 @@ $bin = $_POST['bin'];
 if ($_POST['exists'] == 'Yes') {
   $sqladd = "UPDATE it_inventory SET owned = owned + $amountAdded, available = available + $amountAdded WHERE upc LIKE (?)";
 } else {
-  $sqladd = "INSERT INTO it_inventory (item, category, owned, bin, available, upc) VALUES ('$item', '$category', $amountAdded, '$bin', $amountAdded, (?))";
+  $sqlcheck = "SELECT id FROM it_inventory WHERE upc LIKE (?)";
+  if ($sqlcheck) {
+    echo "<h3>You are attempting to add a duplicate UPC. Please select 'yes' or regenerate UPC.</h3>";
+  } else {
+    $sqladd = "INSERT INTO it_inventory (item, category, owned, bin, available, upc) VALUES ('$item', '$category', $amountAdded, '$bin', $amountAdded, (?))";
+  }
 }
 
 $params = array($upc);
-// $sqldata = sqlsrv_query($conn, $sqladd, $params) or die(sqlsrv_errors());
-
-if ($sqldata === false) {
-  die(echo "UPC already exists in database. Item not added, please select 'yes' to add to an existing item's data.");
-}
+$sqldata = sqlsrv_query($conn, $sqladd, $params) or die(sqlsrv_errors());
 
 header("Location: /../confirm-add.html");
 
