cfae4caec9546d70a16dbcb80eb4b14aec7ad045
Haris <harisawan03@gmail.com>
2020-01-07 16:17:05 -0600

    update text to varchar in SQL files (already reflected in database)

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,16 +1,16 @@
 CREATE TABLE it_inventory
 (
   id INTEGER IDENTITY PRIMARY KEY,
-  item TEXT,
-  category TEXT,
+  item VARCHAR(MAX),
+  category VARCHAR(MAX),
   owned INTEGER,
   available INTEGER,
-  upc TEXT,
+  upc VARCHAR(10),
   bin INTEGER
 );
 
 CREATE TABLE employees
 (
   id INTEGER PRIMARY KEY,
-  name TEXT
+  name VARCHAR(MAX)
 );
