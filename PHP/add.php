33e865158fa17490421addeb39c8853d5db7928f
Haris <harisawan03@gmail.com>
2020-01-21 10:06:27 -0600

    fix syntax error in add.php

--- PHP/add.php
+++ PHP/add.php
@@ -11,7 +11,7 @@ $category = $_POST['category'];
 $bin = $_POST['bin'];
 
 // need to figure out how to determine if need to update or insert based on new way of filling out form from UPC
-if ($item == "") {
+if ($item === "") {
   $sqladd = "INSERT INTO it_inventory (item, category, owned, bin, available, upc) VALUES ('$item', '$category', $amountAdded, '$bin', $amountAdded, (?))";
 } else {
   $sqladd = "UPDATE it_inventory SET owned = owned + $amountAdded, available = available + $amountAdded WHERE upc LIKE (?)";
