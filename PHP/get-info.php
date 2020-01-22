db6f0f3bc421b1e1e85327f553b76046239eba2d
Haris <harisawan03@gmail.com>
2020-01-22 10:29:00 -0600

    work on delay of displaying info on add-inventory

--- PHP/get-info.php
+++ PHP/get-info.php
@@ -9,7 +9,6 @@ $sqlget = "SELECT item, category, available, owned, bin FROM it_inventory WHERE 
 $params = array($upc);
 $sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
 $inventory = sqlsrv_fetch_array($sqldata);
-sleep(5);
 
 echo 'UPC: ' . $upc;
 
