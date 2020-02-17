d98fde63962f23e6f232104fff1da045b092e5b6
Haris <harisawan03@gmail.com>
2020-02-17 14:45:25 -0600

    fix syntax in update-out.php

--- PHP/update-out.php
+++ PHP/update-out.php
@@ -10,13 +10,21 @@ $person = $_POST["name"];
 $sqlupdate = "UPDATE it_inventory SET available = available - 1 WHERE upc LIKE (?)";
 $params = array($upc);
 
-$sqlemployeeid = "SELECT id FROM employees WHERE name = 'Haris'";
-$sqlitemid = "SELECT id FROM it_inventory WHERE upc LIKE '123456789012'";
-$sqlout = "INSERT INTO checked_out VALUES ('$sqlemployeeid', '$sqlitemid')";
-
-
 $sqldata = sqlsrv_query($conn, $sqlupdate, $params);
 if ($sqldata === false) {
     die( print_r( sqlsrv_errors(), true));
 }
+
+$sqlemployeeid = "SELECT id FROM employees WHERE name = $person";
+$sqleid = sqlsrv_query($conn, $sqlemployeeid);
+
+$sqlitemid = "SELECT id FROM it_inventory WHERE upc LIKE (?))";
+$sqliid = sqlsrv_query($conn, $sqlitemid, $params);
+
+$sqlout = "INSERT INTO checked_out VALUES ('$date', '$sqlemployeeid', '$sqlitemid')";
+$sqlcheckout = sqlsrv_query($conn, $sqlupdate, $params);
+if ($sqlcheckout === false) {
+    die( print_r( sqlsrv_errors(), true));
+}
+
 ?>
