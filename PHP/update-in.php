0e3db9a444fa169388001fb3773825274af11cc5
Haris <harisawan03@gmail.com>
2020-02-17 17:25:41 -0600

    extend tracking to check in

--- PHP/update-in.php
+++ PHP/update-in.php
@@ -2,12 +2,23 @@
 $file = 'db-connect.php';
 include $file;
 
-$upc = $_COOKIE["upc"];
+
+
+$upc1 = $_COOKIE["upc"];
+$upc2 = $_COOKIE["upc"];
+$person = $_COOKIE["name"];
 
 $sqlupdate = "UPDATE it_inventory SET available = available + 1 WHERE upc LIKE (?)";
-$params = array($upc);
-$sqldata = sqlsrv_query($conn, $sqlupdate, $params);
+$params1 = array($upc1);
+$sqldata = sqlsrv_query($conn, $sqlupdate, $params1);
 if ($sqldata === false) {
+    die( print_r( sqlsrv_errors(), true));
+}
+
+$sqlin = "INSERT INTO item_tracker VALUES ('checked in', '$date', (SELECT id FROM employees WHERE name = (?)), (SELECT id FROM it_inventory WHERE upc LIKE (?)))";
+$params2 = array($person, $upc2);
+$sqlcheckin = sqlsrv_query($conn, $sqlin, $params2);
+if ($sqlcheckin === false) {
     die( print_r( sqlsrv_errors(), true));
 }
 ?>
--- PHP/update-out.php
+++ PHP/update-out.php
@@ -10,13 +10,12 @@ $person = $_COOKIE["name"];
 
 $sqlupdate = "UPDATE it_inventory SET available = available - 1 WHERE upc LIKE (?)";
 $params1 = array($upc1);
-
 $sqldata = sqlsrv_query($conn, $sqlupdate, $params1);
 if ($sqldata === false) {
     die( print_r( sqlsrv_errors(), true));
 }
 
-$sqlout = "INSERT INTO checked_out VALUES ('$date', (SELECT id FROM employees WHERE name = (?)), (SELECT id FROM it_inventory WHERE upc LIKE (?)))";
+$sqlout = "INSERT INTO item_tracker VALUES ('checked out', '$date', (SELECT id FROM employees WHERE name = (?)), (SELECT id FROM it_inventory WHERE upc LIKE (?)))";
 $params2 = array($person, $upc2);
 $sqlcheckout = sqlsrv_query($conn, $sqlout, $params2);
 if ($sqlcheckout === false) {
