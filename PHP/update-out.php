08ce4c6e308e0db73aa12092a024cb7c00b0eb75
Haris <harisawan03@gmail.com>
2020-02-17 15:13:21 -0600

    fix syntax in update-out.php

--- PHP/update-out.php
+++ PHP/update-out.php
@@ -15,14 +15,14 @@ if ($sqldata === false) {
     die( print_r( sqlsrv_errors(), true));
 }
 
-$sqlemployeeid = "SELECT id FROM employees WHERE name = '$person'";
+$sqlemployeeid = "(SELECT id FROM employees WHERE name = '$person')";
 // $sqleid = sqlsrv_query($conn, $sqlemployeeid);
 
-$sqlitemid = "SELECT id FROM it_inventory WHERE upc LIKE (?))";
+$sqlitemid = "(SELECT id FROM it_inventory WHERE upc LIKE (?))";
 // $sqliid = sqlsrv_query($conn, $sqlitemid, $params);
 
 $sqlout = "INSERT INTO checked_out VALUES ('$date', '$sqlemployeeid', '$sqlitemid')";
-$sqlcheckout = sqlsrv_query($conn, $sqlout);
+$sqlcheckout = sqlsrv_query($conn, $sqlout, $params);
 if ($sqlcheckout === false) {
     die( print_r( sqlsrv_errors(), true));
 }
