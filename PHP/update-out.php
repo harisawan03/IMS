4b16b64323edea5bb9fb97264c4e600aa7c7670c
Haris <harisawan03@gmail.com>
2020-02-17 15:26:11 -0600

    fix syntax in update-out.php

--- PHP/update-out.php
+++ PHP/update-out.php
@@ -4,11 +4,12 @@ include $file;
 
 $date = date("Y-m-d H:i:s");
 
-$upc = $_COOKIE["upc"];
+$upc1 = $_COOKIE["upc"];
+$upc2 = $_COOKIE["upc"];
 $person = $_POST["name"];
 
 $sqlupdate = "UPDATE it_inventory SET available = available - 1 WHERE upc LIKE (?)";
-$params1 = array($upc);
+$params1 = array($upc1);
 
 $sqldata = sqlsrv_query($conn, $sqlupdate, $params1);
 if ($sqldata === false) {
@@ -22,7 +23,7 @@ if ($sqldata === false) {
 // $sqliid = sqlsrv_query($conn, $sqlitemid, $params);
 
 $sqlout = "INSERT INTO checked_out VALUES ('$date', (SELECT id FROM employees WHERE name = (?))), (SELECT id FROM it_inventory WHERE upc LIKE (?)))";
-$params2 = array($person, $upc);
+$params2 = array($person, $upc2);
 $sqlcheckout = sqlsrv_query($conn, $sqlout, $params);
 if ($sqlcheckout === false) {
     die( print_r( sqlsrv_errors(), true));
