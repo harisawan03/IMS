99d4bfb8782a1fa30c5067e5ff99ea684e664e52
Haris <harisawan03@gmail.com>
2020-02-24 09:20:44 -0600

    first attempt at add log

--- PHP/add.php
+++ PHP/add.php
@@ -3,11 +3,14 @@
 $file = 'db-connect.php';
 include $file;
 
-$upc = $_COOKIE["upc"];
+$date = date("Y-m-d H:i:s");
+
+$upc1 = $_COOKIE["upc"];
+$upc2 = $_COOKIE["upc"];
 
 $sqlget = "SELECT item, category, available, owned, bin FROM it_inventory WHERE upc LIKE (?)";
-$params = array($upc);
-$sqldata = sqlsrv_query($conn, $sqlget, $params) or die( print_r( sqlsrv_errors(), true));
+$params1 = array($upc1);
+$sqldata = sqlsrv_query($conn, $sqlget, $params1) or die( print_r( sqlsrv_errors(), true));
 $inventory = sqlsrv_fetch_array($sqldata);
 
 $amountAdded = $_POST['amount'];
@@ -21,11 +24,18 @@ if ($inventory['item']) {
   $sqladd = "INSERT INTO it_inventory (item, category, owned, bin, available, upc) VALUES ('$item', '$category', $amountAdded, '$bin', $amountAdded, (?))";
 }
 
-$params = array($upc);
-$sqldata = sqlsrv_query($conn, $sqladd, $params);
+$params1 = array($upc1);
+$sqldata = sqlsrv_query($conn, $sqladd, $params1);
+
 if ($sqldata === false) {
   header("Location: /../warning.html"); // should not come into play due to automatically detecting if item exists or not, but keep as a failsafe
 } else {
+  $sqlalog = "INSERT INTO add_remove_log VALUES ('added', '$date', (SELECT id FROM it_inventory WHERE upc LIKE (?)), $amountAdded, 'NA')";
+  $params2 = array($upc2);
+  $sqllog = sqlsrv_query($conn, $sqlalog, $params2);
+  if ($sqllog === false) {
+    die( print_r( sqlsrv_errors(), true));
+}
   header("Location: /../confirm-add.html");
 }
 
--- PHP/remove.php
+++ PHP/remove.php
@@ -10,18 +10,18 @@ $upc2 = $_COOKIE["upc"];
 
 $amountRemoved = $_POST['amount'];
 $reason = $_POST['reason'];
-echo "test1";
+
 $sqlremove = "UPDATE it_inventory SET owned = owned - $amountRemoved, available = available - $amountRemoved WHERE upc LIKE (?)"; // figure out how removal will work if item is associated with an employee
 $params1 = array($upc1);
 $sqldata = sqlsrv_query($conn, $sqlremove, $params1) or die(sqlsrv_errors());
-echo "test2";
+
 $sqlrlog = "INSERT INTO add_remove_log VALUES ('removed', '$date', (SELECT id FROM it_inventory WHERE upc LIKE (?)), $amountRemoved, '$reason')";
 $params2 = array($upc2);
 $sqllog = sqlsrv_query($conn, $sqlrlog, $params2);
 if ($sqllog === false) {
     die( print_r( sqlsrv_errors(), true));
 }
-echo "test3";
+
 header("Location: /../confirm-remove.html");
 
 ?>
