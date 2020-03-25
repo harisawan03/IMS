17ed493d3cc3dcbf7e0e72b19bd6224d1b731062
Hairs Awan <harisawan@rvc.local>
2020-03-25 13:04:51 -0500

    update sql for viewing data

--- SQL/view_data.sql
+++ SQL/view_data.sql
@@ -1,7 +1,6 @@
 SELECT * FROM it_inventory;
 SELECT * FROM employees;
-SELECT item, owned, available FROM it_inventory WHERE upc LIKE '123456789012';
-SELECT item, owned, available FROM it_inventory WHERE upc LIKE '109876543281';
+
 SELECT * FROM item_tracker;
 SELECT * FROM add_remove_log;
 
@@ -9,3 +8,7 @@ SELECT employees.name, item_tracker.action, it_inventory.item, item_tracker.date
 FROM item_tracker
 INNER JOIN employees ON item_tracker.employee_id = employees.id
 INNER JOIN it_inventory ON item_tracker.item_id = it_inventory.id;
+
+SELECT add_remove_log.amount, it_inventory.item, add_remove_log.action, add_remove_log.date, add_remove_log.reason
+FROM add_remove_log
+INNER JOIN it_inventory ON add_remove_log.item_id = it_inventory.id;
