62ff02764c952163aede662af3905c9e51db07c6
Haris <harisawan03@gmail.com>
2021-07-05 14:43:48 -0500

    add ability to connect to demo server

--- PHP/db-connect.php
+++ PHP/db-connect.php
@@ -3,13 +3,20 @@
 // used for all php scripts to make connection to database
 
 $serverName = "RVC-INVENTORY";
+$serverNameDemo = "HARIS-PC"; 
 
 $connectionInfo = array( "Database"=>"RVC-INVENTORY");
+$connectionInfoDemo = array( "Database"=>"IMS-DEMO");
+
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( !$conn ) {
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+     $conn = sqlsrv_connect( $serverNameDemo, $connectionInfoDemo);
+
+     if ( !$conn ) {
+          echo "Connection could not be established.<br />";
+          die( print_r( sqlsrv_errors(), true));
+     }
 }
 
 ?>
